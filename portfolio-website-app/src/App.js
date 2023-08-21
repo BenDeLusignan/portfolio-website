28313ea083a48ddd4ff944e743295bb36fb9ec39
Ben de Lusignan <bendelusignan@gmail.com>
2023-08-21 01:44:31 +0100

    Continued work on CSS for landing page.

--- portfolio-website-app/src/App.js
+++ portfolio-website-app/src/App.js
@@ -8,30 +8,32 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      
       <body>
         <h1>Ben de Lusignan</h1>
-          <Router>
-            <nav >
-              <ul id="navbar">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                  <Link to="/python">Python</Link>
-                </li>
-              </ul>
-            </nav>
-            <Routes>
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/python" element={<Python />} />
-              <Route path="/" element={<Homepage />} />
-            </Routes>
-          </Router>
-        
+        <Router>
+          <nav>
+            <ul id="navbar">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/python">Python</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/python" element={<Python />} />
+            <Route path="/" element={<Homepage />} />
+          </Routes>
+        </Router>
+
+        <footer id="footer">
+          Ben de Lusignan | 2023
+        </footer>
       </body>
     </div>
   );
